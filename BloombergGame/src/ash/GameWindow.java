fe6f8afd819b13ce79dfc691e60e4d3956292fb6
ashersebban <61130337+ashersebban@users.noreply.github.com>
2021-01-15 15:31:16 -0800

    added changing shapes with spacebar and primitive jumping capability... will add gravity and different movement options for each shape next time.

--- BloombergGame/src/ash/GameWindow.java
+++ BloombergGame/src/ash/GameWindow.java
@@ -15,6 +15,11 @@ public class GameWindow extends PApplet {
 	final private int w = 600;
 	final private int h = 400;
 
+	final private int ceiling = 0;
+	final private int floor = h;
+	final private int leftWall = 0;
+	final private int rightWall = w;
+	int counter = 0;
 	
 	
 	/**
@@ -35,13 +40,25 @@ public class GameWindow extends PApplet {
 
 	}
 	
+	int vertical = 10;
+	
+	int playerWidth = 50;
+	int playerHeight = 50;
+	int playerX = leftWall + 50;
+	int playerY = floor - playerHeight - 10;
+	String playerShape = "Square";
+	
+	boolean jumping = false;
+	boolean falling = false; 
+	
+	int[] color = {255,0,0};
+	Square user = new Square("User",playerX,playerY,playerWidth,playerHeight,color);
+	
 	/**
 	 * Method to compose the first 'frame' of the app
 	 */
 	public void setup() {
 		
-		int[] color = {255,0,255};
-		Square square1 = new Square("Square1",h/2,w/2,100,100,color);
 		
 		//fill the background with this color (specified in 8-bit R, G, B values)
 		this.background(255, 255, 255); //white
@@ -49,30 +66,76 @@ public class GameWindow extends PApplet {
 		//set the fill color (the color which solid shapes will be filled with)
 		this.fill(color[0],color[1],color[2]); 
 
-		public void keyPressed() {
-			if (key == ' ') {
-				square1.setPosY(square1.getPosY()+30);
-			}
-		}
 	}
 	
-
+	public void keyPressed() {
+		 if(key == ' ') {
+			if(playerShape.equalsIgnoreCase("Square")) {
+				playerShape = "Circle";
+			}
+			else if (playerShape.equalsIgnoreCase("Circle")) {
+				playerShape = "Triangle";
+			}
+			else if (playerShape.equalsIgnoreCase("Triangle")) {
+				playerShape = "Square";
+			}
+			
+		}
+		if(key == CODED) {
+			if(!jumping && !falling && keyCode == UP) {
+				jumping = true;
+			}
+		}
+		
+	}
+	
 	
 	/**
 	 * This method is called repeatedly many times per second (usually 30 times per second by default) for the lifetime of the app.
 	 */
 	public void draw() {
 		
-		//set the fill color (the color which solid shapes will be filled with)
-				this.fill(color[0],color[1],color[2]); 
-				
+		ellipseMode(CORNER);
+		
 		//fill the background with this color (specified in 8-bit R, G, B values)
 		this.background(255, 255, 255); //white
 		
-		rect(square1.getPosX(),square1.getPosY(),square1.sizeX,square1.sizeY);
+		//set the fill color (the color which solid shapes will be filled with)
+		this.fill(color[0],color[1],color[2]);
 		
+		if(playerShape.equalsIgnoreCase("Square"))rect(playerX,playerY,playerWidth,playerHeight);
+		else if(playerShape.equalsIgnoreCase("Circle"))ellipse(playerX,playerY,playerWidth,playerHeight);
+		else if(playerShape.equalsIgnoreCase("Triangle"))triangle(playerX,playerY+playerHeight,playerWidth+playerX,playerY+playerHeight,playerX+playerWidth/2,playerY);
+		
+		if(jumping) {
+			playerY -= vertical;
+			falling = false;
+			
+		}
+		else if(falling) {
+			playerY += vertical;
 		}
 		
+		int topOfPlayer = playerY;
+		int bottomOfPlayer = playerY + playerHeight;
+		
+		if(topOfPlayer <= ceiling) {
+			jumping = false;
+			falling = true;
+			System.out.println();
+			System.out.println(topOfPlayer + " | "+ceiling);
+
+			
+		}
+		
+		if(bottomOfPlayer >= floor-10) {
+			jumping = false;
+			falling = false;
+			System.out.println();
+			System.out.println(bottomOfPlayer + " | "+floor);
+			playerY = floor - playerHeight - 10;
+		}
 		
 	}
+}
 	
